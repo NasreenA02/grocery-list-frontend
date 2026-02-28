234dc14c073c4a91f1f9fdfdcb82b3e7c9386b22
NasreenA02 <nassu324@gmail.com>
2026-02-28 14:47:41 +0530

    connect frontend to deployed backend

--- Grocery-list-frontend/src/services/api.js
+++ Grocery-list-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://grocery-list-backend-h6ww.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
